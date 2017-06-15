ba7972cdebd6028d25b9470830b0704a87c75f23
OrestiArsi <orestiarsi@gmail.com>
2017-06-15 14:09:26 -0400

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 function handValue(hand){
-
   let value =0;
   hand.sort(card => card === "A" ? 1:-1);
-  for (i=0; i< hand.length; i++)
-  {
-
-    if ((hand[i] == "2") || (hand[i] == "3") || (hand[i] == "4") || (hand[i]== "5") || (hand[i] == "6") || (hand[i] == "7") || (hand[i] == "8") || (hand[i]== "9") || (hand[i] == "10")){
+  for (i=0; i< hand.length; i++) {
+    if ((hand[i] == "2") || (hand[i] == "3") || (hand[i] == "4") || (hand[i]== "5") || (hand[i] == "6") || (hand[i] == "7") || (hand[i] == "8") || (hand[i]== "9") || (hand[i] == "10")) {
       value += parseInt(hand[i]);}
-
-       else if ((hand [i] == "K") || (hand[i] == "Q") || (hand[i] =="J")){ value +=10;}
-      else if (value>10) {value+=1;}
-      }console.log (value);
+    else if ((hand [i] == "K") || (hand[i] == "Q") || (hand[i] =="J")) {
+      value +=10;
     }
+    else if (value>10) {
+      value+=1;
+    }
+    else {
+      value+=11;
+    }
+  }
+  return value;
+}
 
 
 
